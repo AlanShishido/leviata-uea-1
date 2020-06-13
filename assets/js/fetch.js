dd5130627e474b6611b169a0229071ffb11aa4c6
lpgoulart <leonardo@roffor.com.br>
2020-06-13 00:18:02 -0300

    ajustes de responsividade

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -109,3 +109,49 @@ function addCard(object) {
 // <div style="text-align: left; font-size: 23px; font-family: 'Comic Neue', cursive; font-weight: 800">
 //     <i class="far fa-heart"></i> <span>${object.likes}</span>&nbsp;<i class="far fa-comment"></i> <span>${object.comments}</span>
 // </div>
+
+window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+    
+    var form = document.getElementById("my-form");
+    var button = document.getElementById("my-form-button");
+    var status = document.getElementById("my-form-status");
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      button.style = "display: none ";
+      status.innerHTML = "Obrigado! Logo entraremos em contato.";
+    }
+
+    function error() {
+      status.innerHTML = "Parece que teve um problema!!";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
